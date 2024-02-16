72e7ada8ac8327316f5ac8572d1a2936956a07d5
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-02-16 20:27:12 +0200

    adding windows form app

---
+++ File Management Class/Login.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace File_Management_Class
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            linkLabel1 = new LinkLabel();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(299, 284);
+            button1.Name = "button1";
+            button1.Size = new Size(186, 52);
+            button1.TabIndex = 0;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(118, 102);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 17);
+            label1.TabIndex = 1;
+            label1.Text = "Email:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(118, 145);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 17);
+            label2.TabIndex = 2;
+            label2.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(262, 102);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(468, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(262, 143);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(468, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(330, 235);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(114, 17);
+            linkLabel1.TabIndex = 6;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Forget Password";
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(800, 382);
+            Controls.Add(linkLabel1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Font = new Font("Microsoft Sans Serif", 8.25F);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            WindowState = FormWindowState.Maximized;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private LinkLabel linkLabel1;
+    }
+}
---
+++ File Management Class/Login.cs
@@ -0,0 +1,25 @@
+namespace File_Management_Class
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
