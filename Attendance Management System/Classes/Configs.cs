654d1a67f833674076f7b315476cbf95a9d1a10b
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-02-17 14:04:53 +0200

    Correct project namming and name space

---
+++ Attendance Management System/Classes/Configs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attendance_Management_System.Classes
+{
+    internal class Configs
+    {
+        public static string DataPath = @"../../../Data/Data.xml";
+        public static string UsersPath = "/school/users/user";
+        public static string language = "EN";
+        public static string dateFormate = "dd/MM/yyyy";
+    }
+}
---
+++ Attendance Management System/Forms/Admin.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Attendance_Management_System
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openFileToolStripMenuItem = new ToolStripMenuItem();
+            saveFileToolStripMenuItem = new ToolStripMenuItem();
+            logoutToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            addToolStripMenuItem = new ToolStripMenuItem();
+            studentToolStripMenuItem = new ToolStripMenuItem();
+            teacherToolStripMenuItem = new ToolStripMenuItem();
+            classToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            studentToolStripMenuItem1 = new ToolStripMenuItem();
+            teacherToolStripMenuItem1 = new ToolStripMenuItem();
+            classToolStripMenuItem1 = new ToolStripMenuItem();
+            searchToolStripMenuItem = new ToolStripMenuItem();
+            studentToolStripMenuItem2 = new ToolStripMenuItem();
+            idToolStripMenuItem = new ToolStripMenuItem();
+            nameToolStripMenuItem = new ToolStripMenuItem();
+            courseToolStripMenuItem1 = new ToolStripMenuItem();
+            teacherToolStripMenuItem2 = new ToolStripMenuItem();
+            idToolStripMenuItem1 = new ToolStripMenuItem();
+            nameToolStripMenuItem1 = new ToolStripMenuItem();
+            courseToolStripMenuItem = new ToolStripMenuItem();
+            classToolStripMenuItem2 = new ToolStripMenuItem();
+            idToolStripMenuItem2 = new ToolStripMenuItem();
+            nameToolStripMenuItem2 = new ToolStripMenuItem();
+            reportToolStripMenuItem = new ToolStripMenuItem();
+            byClassToolStripMenuItem = new ToolStripMenuItem();
+            byClassToolStripMenuItem1 = new ToolStripMenuItem();
+            byDateRangeToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, addToolStripMenuItem, editToolStripMenuItem, searchToolStripMenuItem, reportToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1422, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openFileToolStripMenuItem, saveFileToolStripMenuItem, logoutToolStripMenuItem, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            openFileToolStripMenuItem.Size = new Size(224, 26);
+            openFileToolStripMenuItem.Text = "Open File";
+            // 
+            // saveFileToolStripMenuItem
+            // 
+            saveFileToolStripMenuItem.Name = "saveFileToolStripMenuItem";
+            saveFileToolStripMenuItem.Size = new Size(224, 26);
+            saveFileToolStripMenuItem.Text = "Save File";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            logoutToolStripMenuItem.Size = new Size(224, 26);
+            logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(224, 26);
+            exitToolStripMenuItem.Text = "Exit";
+            // 
+            // addToolStripMenuItem
+            // 
+            addToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { studentToolStripMenuItem, teacherToolStripMenuItem, classToolStripMenuItem });
+            addToolStripMenuItem.Name = "addToolStripMenuItem";
+            addToolStripMenuItem.Size = new Size(51, 24);
+            addToolStripMenuItem.Text = "Add";
+            // 
+            // studentToolStripMenuItem
+            // 
+            studentToolStripMenuItem.Name = "studentToolStripMenuItem";
+            studentToolStripMenuItem.Size = new Size(224, 26);
+            studentToolStripMenuItem.Text = "Student";
+            // 
+            // teacherToolStripMenuItem
+            // 
+            teacherToolStripMenuItem.Name = "teacherToolStripMenuItem";
+            teacherToolStripMenuItem.Size = new Size(224, 26);
+            teacherToolStripMenuItem.Text = "Teacher";
+            // 
+            // classToolStripMenuItem
+            // 
+            classToolStripMenuItem.Name = "classToolStripMenuItem";
+            classToolStripMenuItem.Size = new Size(224, 26);
+            classToolStripMenuItem.Text = "Class";
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { studentToolStripMenuItem1, teacherToolStripMenuItem1, classToolStripMenuItem1 });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(49, 24);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // studentToolStripMenuItem1
+            // 
+            studentToolStripMenuItem1.Name = "studentToolStripMenuItem1";
+            studentToolStripMenuItem1.Size = new Size(143, 26);
+            studentToolStripMenuItem1.Text = "Student";
+            // 
+            // teacherToolStripMenuItem1
+            // 
+            teacherToolStripMenuItem1.Name = "teacherToolStripMenuItem1";
+            teacherToolStripMenuItem1.Size = new Size(143, 26);
+            teacherToolStripMenuItem1.Text = "Teacher";
+            // 
+            // classToolStripMenuItem1
+            // 
+            classToolStripMenuItem1.Name = "classToolStripMenuItem1";
+            classToolStripMenuItem1.Size = new Size(143, 26);
+            classToolStripMenuItem1.Text = "Class";
+            // 
+            // searchToolStripMenuItem
+            // 
+            searchToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { studentToolStripMenuItem2, teacherToolStripMenuItem2, classToolStripMenuItem2 });
+            searchToolStripMenuItem.Name = "searchToolStripMenuItem";
+            searchToolStripMenuItem.Size = new Size(67, 24);
+            searchToolStripMenuItem.Text = "Search";
+            // 
+            // studentToolStripMenuItem2
+            // 
+            studentToolStripMenuItem2.DropDownItems.AddRange(new ToolStripItem[] { idToolStripMenuItem, nameToolStripMenuItem, courseToolStripMenuItem1 });
+            studentToolStripMenuItem2.Name = "studentToolStripMenuItem2";
+            studentToolStripMenuItem2.Size = new Size(143, 26);
+            studentToolStripMenuItem2.Text = "Student";
+            // 
+            // idToolStripMenuItem
+            // 
+            idToolStripMenuItem.Name = "idToolStripMenuItem";
+            idToolStripMenuItem.Size = new Size(152, 26);
+            idToolStripMenuItem.Text = "By Id";
+            // 
+            // nameToolStripMenuItem
+            // 
+            nameToolStripMenuItem.Name = "nameToolStripMenuItem";
+            nameToolStripMenuItem.Size = new Size(152, 26);
+            nameToolStripMenuItem.Text = "By Name";
+            // 
+            // courseToolStripMenuItem1
+            // 
+            courseToolStripMenuItem1.Name = "courseToolStripMenuItem1";
+            courseToolStripMenuItem1.Size = new Size(152, 26);
+            courseToolStripMenuItem1.Text = "By Class";
+            courseToolStripMenuItem1.Click += courseToolStripMenuItem1_Click;
+            // 
+            // teacherToolStripMenuItem2
+            // 
+            teacherToolStripMenuItem2.DropDownItems.AddRange(new ToolStripItem[] { idToolStripMenuItem1, nameToolStripMenuItem1, courseToolStripMenuItem });
+            teacherToolStripMenuItem2.Name = "teacherToolStripMenuItem2";
+            teacherToolStripMenuItem2.Size = new Size(143, 26);
+            teacherToolStripMenuItem2.Text = "Teacher";
+            teacherToolStripMenuItem2.Click += teacherToolStripMenuItem2_Click;
+            // 
+            // idToolStripMenuItem1
+            // 
+            idToolStripMenuItem1.Name = "idToolStripMenuItem1";
+            idToolStripMenuItem1.Size = new Size(157, 26);
+            idToolStripMenuItem1.Text = "By Id";
+            // 
+            // nameToolStripMenuItem1
+            // 
+            nameToolStripMenuItem1.Name = "nameToolStripMenuItem1";
+            nameToolStripMenuItem1.Size = new Size(157, 26);
+            nameToolStripMenuItem1.Text = "By Name";
+            // 
+            // courseToolStripMenuItem
+            // 
+            courseToolStripMenuItem.Name = "courseToolStripMenuItem";
+            courseToolStripMenuItem.Size = new Size(157, 26);
+            courseToolStripMenuItem.Text = "By Course";
+            // 
+            // classToolStripMenuItem2
+            // 
+            classToolStripMenuItem2.DropDownItems.AddRange(new ToolStripItem[] { idToolStripMenuItem2, nameToolStripMenuItem2 });
+            classToolStripMenuItem2.Name = "classToolStripMenuItem2";
+            classToolStripMenuItem2.Size = new Size(143, 26);
+            classToolStripMenuItem2.Text = "Class";
+            // 
+            // idToolStripMenuItem2
+            // 
+            idToolStripMenuItem2.Name = "idToolStripMenuItem2";
+            idToolStripMenuItem2.Size = new Size(152, 26);
+            idToolStripMenuItem2.Text = "By Id";
+            // 
+            // nameToolStripMenuItem2
+            // 
+            nameToolStripMenuItem2.Name = "nameToolStripMenuItem2";
+            nameToolStripMenuItem2.Size = new Size(152, 26);
+            nameToolStripMenuItem2.Text = "By Name";
+            // 
+            // reportToolStripMenuItem
+            // 
+            reportToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { byClassToolStripMenuItem, byClassToolStripMenuItem1, byDateRangeToolStripMenuItem });
+            reportToolStripMenuItem.Name = "reportToolStripMenuItem";
+            reportToolStripMenuItem.Size = new Size(68, 24);
+            reportToolStripMenuItem.Text = "Report";
+            // 
+            // byClassToolStripMenuItem
+            // 
+            byClassToolStripMenuItem.Name = "byClassToolStripMenuItem";
+            byClassToolStripMenuItem.Size = new Size(190, 26);
+            byClassToolStripMenuItem.Text = "By Student";
+            // 
+            // byClassToolStripMenuItem1
+            // 
+            byClassToolStripMenuItem1.Name = "byClassToolStripMenuItem1";
+            byClassToolStripMenuItem1.Size = new Size(190, 26);
+            byClassToolStripMenuItem1.Text = "By Class";
+            // 
+            // byDateRangeToolStripMenuItem
+            // 
+            byDateRangeToolStripMenuItem.Name = "byDateRangeToolStripMenuItem";
+            byDateRangeToolStripMenuItem.Size = new Size(190, 26);
+            byDateRangeToolStripMenuItem.Text = "By Date Range";
+            // 
+            // Admin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1422, 853);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Admin";
+            Text = "Admin";
+            Load += Admin_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openFileToolStripMenuItem;
+        private ToolStripMenuItem saveFileToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem addToolStripMenuItem;
+        private ToolStripMenuItem studentToolStripMenuItem;
+        private ToolStripMenuItem teacherToolStripMenuItem;
+        private ToolStripMenuItem classToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem studentToolStripMenuItem1;
+        private ToolStripMenuItem teacherToolStripMenuItem1;
+        private ToolStripMenuItem classToolStripMenuItem1;
+        private ToolStripMenuItem searchToolStripMenuItem;
+        private ToolStripMenuItem studentToolStripMenuItem2;
+        private ToolStripMenuItem idToolStripMenuItem;
+        private ToolStripMenuItem nameToolStripMenuItem;
+        private ToolStripMenuItem courseToolStripMenuItem1;
+        private ToolStripMenuItem teacherToolStripMenuItem2;
+        private ToolStripMenuItem idToolStripMenuItem1;
+        private ToolStripMenuItem nameToolStripMenuItem1;
+        private ToolStripMenuItem courseToolStripMenuItem;
+        private ToolStripMenuItem classToolStripMenuItem2;
+        private ToolStripMenuItem idToolStripMenuItem2;
+        private ToolStripMenuItem nameToolStripMenuItem2;
+        private ToolStripMenuItem logoutToolStripMenuItem;
+        private ToolStripMenuItem reportToolStripMenuItem;
+        private ToolStripMenuItem byClassToolStripMenuItem;
+        private ToolStripMenuItem byClassToolStripMenuItem1;
+        private ToolStripMenuItem byDateRangeToolStripMenuItem;
+    }
+}
